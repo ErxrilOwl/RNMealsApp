eb00932d6b064b263f65d068c3697803669cc6de
Nikko <atuan.nikko@gmail.com>
2026-05-31 21:48:19 +0800

    feat: initial commit

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,17 @@
+import { FlatList } from 'react-native';
+import { CategoryGridTile } from '../components/CategoryGridTile';
+import { CATEGORIES } from '../data/dummy-data';
+
+export const CategoriesScreen = () => {
+    const renderCategoryItem = itemData => {
+        return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+    }
+
+    return (
+        <FlatList 
+            data={CATEGORIES} 
+            keyExtractor={item => item.id} 
+            renderItem={renderCategoryItem}
+        />
+    )
+}
