7cc8b5bbbf2a91b77db5bc58cb99288afb780410
Nikko <atuan.nikko@gmail.com>
2026-06-01 21:33:31 +0800

    feat: added page navigation

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,13 @@
 import { Pressable, StyleSheet, Text, View, Platform } from "react-native"
+// import { useNavigation } from "@react-navigation/native";
 
-export const CategoryGridTile = ({ title, color }) => {
+export const CategoryGridTile = ({ title, color, onPress }) => {
     return (
         <View style={styles.gridItem}>
             <Pressable 
                 style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null ]} 
-                android_ripple={{ color: '#ccc' }}>
+                android_ripple={{ color: '#ccc' }}
+                onPress={onPress}>
                 <View style={[styles.innerContainer, { backgroundColor: color }]}>
                     <Text style={styles.title}>{title}</Text>
                 </View>
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,9 +2,17 @@ import { FlatList } from 'react-native';
 import { CategoryGridTile } from '../components/CategoryGridTile';
 import { CATEGORIES } from '../data/dummy-data';
 
-export const CategoriesScreen = () => {
+export const CategoriesScreen = ({ navigation }) => {
+
+    const pressHandler = () => {
+        navigation.navigate('MealsOverview')
+    }
+
     const renderCategoryItem = itemData => {
-        return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+        return <CategoryGridTile 
+            title={itemData.item.title} 
+            color={itemData.item.color} 
+            onPress={pressHandler}/>
     }
 
     return (
