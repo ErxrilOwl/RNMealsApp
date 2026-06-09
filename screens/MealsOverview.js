c3e72a2fc213cd2783d218378984460c5ee08879
Nikko <atuan.nikko@gmail.com>
2026-06-09 21:49:09 +0800

    feat: dynamic navigation options

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,15 +1,24 @@
 import { StyleSheet, FlatList, View } from "react-native";
 import { useRoute } from "@react-navigation/native";
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import { MealItem } from "../components/MealItem";
+import { useLayoutEffect } from "react";
 
-export const MealsOverviewScreen = () => {
+export const MealsOverviewScreen = ({ route, navigation }) => {
     const route = useRoute();
     const catId = route.params.categoryId;
 
     const displayedMeals = MEALS.filter((mealItem) => {
         return mealItem.categoryIds.indexOf(catId) >= 0;
     });
+
+    useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find((category) => category.id === catId).title;
+
+        navigation.setOptions({
+            title: categoryTitle
+        });
+    }, [catId, navigation]);
 
     const renderMealItem = ({ item }) => {
         return <MealItem { ...item }/>
