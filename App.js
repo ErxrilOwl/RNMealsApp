4619ce87a4fcf07d86eaa26289669425c26051b7
Nikko <atuan.nikko@gmail.com>
2026-06-22 21:52:10 +0800

    feat: redux init

--- App.js
+++ App.js
@@ -8,7 +8,9 @@ import { MealDetailScreen } from './screens/MealDetailScreen';
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { FavoritesScreen } from './screens/FavoritesScreen';
 import { Ionicons } from '@expo/vector-icons';
-import FavoritesContextProvider from './store/context/favorites-context';
+// import FavoritesContextProvider from './store/context/favorites-context';
+import { Provider } from 'react-redux';
+import { store } from './store/redux/store';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -46,7 +48,8 @@ export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator screenOptions={{
             headerStyle: {
@@ -69,7 +72,8 @@ export default function App() {
             <Stack.Screen name="MealDetail" component={MealDetailScreen} options={{ title: 'About the Meal' }}/>
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+      </Provider>
+      {/* </FavoritesContextProvider> */}
 
     </>
   );
